3306455ceb3d96caa6ce74faeb4a150b9540c4cb
Turab Manzoor <turabmanzoor032@gmail.com>
2026-03-23 13:01:52 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.status(201).json({
+    message: "note created successfully",
+  });
+});
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "notes fecteched successfully",
+    notes: notes,
+  });
+});
+
+module.exports = app;
