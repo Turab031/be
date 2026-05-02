53657d0a43f91fef28e6eb3db6ce03fdc4acc081
Turab Manzoor <turabmanzoor032@gmail.com>
2026-05-02 13:11:06 +0530

    regiter

--- spotify/src/controllers/auth.controller.js
+++ spotify/src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 async function registerUser(req, res) {
   const { username, email, password, role = "user" } = req.body;
@@ -6,5 +8,54 @@ async function registerUser(req, res) {
     $or: [{ username }, { email }],
   });
 
-  
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      message: "user already exist",
+    });
+  }
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await userModel.create({
+    username,
+    email,
+    password,
+    role,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("token", token);
+  res.status(201).json({
+    message: "user registerd successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  });
 }
+
+async function loginuser(req, res) {
+  const { username, email, password } = req.body;
+  const user = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    res.status(401).json({ message: "user not found" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    res.status(401).json({ message: "invalid credentials" });
+  }
+}
+
+module.exports = { registerUser };
--- spotify/src/routes/auth.routes.js
+++ spotify/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
 
-router.post('/register',)
+router.post("/register", authController.registerUser);
 
 module.exports = router;
