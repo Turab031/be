ec1955a2020bd44f0ca068b9833d4b120bc9b4aa
Turab Manzoor <turabmanzoor032@gmail.com>
2026-05-03 18:02:11 +0530

    schema updated

--- spotify/src/controllers/auth.controller.js
+++ spotify/src/controllers/auth.controller.js
@@ -18,7 +18,7 @@ async function registerUser(req, res) {
   const user = await userModel.create({
     username,
     email,
-    password,
+    password: hash,
     role,
   });
 
@@ -49,13 +49,31 @@ async function loginuser(req, res) {
   });
 
   if (!user) {
-    res.status(401).json({ message: "user not found" });
+    return res.status(401).json({ message: "user not found" });
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
-    res.status(401).json({ message: "invalid credentials" });
+    return res.status(401).json({ message: "invalid credentials" });
   }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  return res.status(200).json({
+    message: "user logged in successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  });
 }
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginuser };
