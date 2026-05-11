b60602621c176d3e966c7a6f2486d2c92e8fccba
Turab Manzoor <turabmanzoor032@gmail.com>
2026-05-11 11:27:42 +0530

    get users

--- system/src/routes/auth.routes.js
+++ system/src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controller");
 const authRouter = express.Router();
 
 authRouter.post("/register", authController.register);
+authRouter.get("/get-me", authController.getMe);
 
 module.exports = authRouter;
